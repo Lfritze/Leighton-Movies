42fde070ab846614bd14c6fa87621148101af0c6
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-04-24 14:09:21 -0400

    setup sign up form in register.php

--- register.php
+++ register.php
@@ -15,6 +15,14 @@
 <body>
   <div class="signInContainer">
     <div class="column">
+
+      <div class="header">
+        <img src="assets/images/logo.png" title="Logo" alt="Site Logo" />
+        <h3>Sign Up</h3>
+        <span>to continue to Leighton-Movies</span>
+
+      </div>
+
       <form method="POST">
         <input type="text" name="firstName" placeholder="First Name" required>
         <input type="text" name="lastName" placeholder="Last Name" required>
@@ -25,6 +33,7 @@
         <input type="password" name="password2" placeholder="Confirm Password" required>
         <input type="submit" name="submitButton" value="SUBMIT">
       </form>
+      <a href="login.php" class="signInMessage">Already have an account? Sign in here!</a>
     </div>
   </div>
 </body>
