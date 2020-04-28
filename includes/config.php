e1340ae7acb6cbeb882808b16abd5ca5f5720179
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-04-27 23:19:16 -0400

    connected to database in config.php

--- includes/config.php
+++ includes/config.php
@@ -2,5 +2,17 @@
 ob_start(); //turns on output buffering
 session_start();
 
-date_default_timezone_set("America/Indiana/Indianapolis")
+date_default_timezone_set("America/Indiana/Indianapolis");
+
+try {
+  $con = new PDO("mysql:dbname=leightonflix;host=localhost", "root", "");
+  $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+
+}
+
+catch (PDOException $e) {
+  exit("Connection failed: " . $e->getMessage());
+
+}
+
 ?>
