7a4f5714f393a4550656868f5c4491a6c9e9ec23
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-04-28 17:50:30 -0400

    setup first name error constant in Constants.php

--- register.php
+++ register.php
@@ -1,6 +1,7 @@
 <?php
 require_once("includes/config.php");
 require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Constants.php");
 require_once("includes/classes/Account.php");
 
 $account = new Account($con);
@@ -40,7 +41,7 @@ $account = new Account($con);
 
       <form method="POST">
 
-        <?php echo $account->getError("First name wrong length"); ?>
+        <?php echo $account->getError(Constants::$firstNameCharacters); ?>
         <input type="text" name="firstName" placeholder="First name" required>
 
         <input type="text" name="lastName" placeholder="Last name" required>
