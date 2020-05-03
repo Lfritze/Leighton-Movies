f6559226db128ea6f8c76f9b6bbe977cd49b32fe
Leighton Fritze <Jay.Row.Fritze@gmail.com>
2020-05-03 12:12:04 -0400

    setup login query

--- login.php
+++ login.php
@@ -1,6 +1,22 @@
 <?php
+require_once("includes/config.php");
+require_once("includes/classes/FormSanitizer.php");
+require_once("includes/classes/Constants.php");
+require_once("includes/classes/Account.php");
+
+$account = new Account($con);
+
   if(isset($_POST["submitButton"])) {
-    echo "Form was Submitted";
+
+        $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+        $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+
+        $success = $account->login($username, $password);
+
+        if($success) {
+          // store session
+          header("Location: index.php");
+        }
   }
 ?>
 
@@ -24,7 +40,8 @@
       </div>
 
       <form method="POST">
-        <input type="text" name="userName" placeholder="Username" required>
+        <?php echo $account->getError(Constants::$loginFailed); ?>
+        <input type="text" name="username" placeholder="Username" required>
         <input type="password" name="password" placeholder="Password" required>
         <input type="submit" name="submitButton" value="SUBMIT">
       </form>
